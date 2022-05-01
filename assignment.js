069cff9cd7916e2e3edca871a38585608daaad13
Nathi Geigant <ngeigant@gmail.com>
2022-05-01 13:56:18 +0200

    Add teacher solution to assignment.js

--- assignment.js
+++ assignment.js
@@ -1,4 +1,6 @@
+// User input
 const myNumber = 35;
+// Result
 let myResult = 18 + myNumber;
 
 myResult = myResult - 40;
@@ -7,3 +9,16 @@ myResult = myResult / 2;
 
 alert(myNumber);
 alert(myResult);
+
+// How the assignment was completed by the teacher
+// const userInput = 20 // Bonus for using const instead of let
+// let result;
+
+// result = 18 + userInput
+
+// result = result - 10
+// result = result * 2
+// result = result / 4
+
+// alert(result)
+// alert(userInput)
